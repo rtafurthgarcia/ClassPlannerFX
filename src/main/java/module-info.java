84400f19b62be296ba7bdb798b1bf410f15deec8
Richard Tafurth-Garcia <projects@cdstm.ch>
2022-12-27 09:55:51 +0100

    Functionning jvm config for controlsfx

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,10 @@ module ch.hftm {
     requires javafx.controls;
     requires javafx.base;
     requires javafx.fxml;
+    requires org.controlsfx.controls;
 
     exports ch.hftm;
-
+    
     opens ch.hftm.controller to javafx.fxml;
     //opens ch.hftm.model to javafx.fxml;
 }
