17f38e5b7dd8517e45b28e2e1857f92010e48764
Richard Tafurth-Garcia <projects@cdstm.ch>
2023-01-09 14:56:00 +0100

    Hope its really needed

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,11 @@ module ch.hftm {
     requires javafx.controls;
     requires javafx.base;
     requires javafx.fxml;
+    requires javafx.graphics;
     requires org.controlsfx.controls;
 
     exports ch.hftm;
-    
+
     opens ch.hftm.controller to javafx.fxml;
     //opens ch.hftm.model to javafx.fxml;
 }
